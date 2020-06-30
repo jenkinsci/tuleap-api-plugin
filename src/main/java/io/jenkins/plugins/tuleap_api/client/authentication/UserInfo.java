ff0d3ef7f4197dd57c41d1d96ec163b389c51e01
robinsoc <quarck@live.fr>
2020-06-30 15:32:31 +0200

    Get the email and the name of the connected user thanks to the UserInfo endpoint (#35)
    This is a part of [request #15033](https://tuleap.net/plugins/tracker/?aid=15033) Improve the Jenkins Authentication plugin
    
    The retrieved information will be used in the Tuleap Authentication plugin
    
    Co-authored-by: Clarck Robinson <clarck.robinson@enalean.com>

--- src/main/java/io/jenkins/plugins/tuleap_api/client/authentication/UserInfo.java
+++ src/main/java/io/jenkins/plugins/tuleap_api/client/authentication/UserInfo.java
@@ -3,4 +3,7 @@ package io.jenkins.plugins.tuleap_api.client.authentication;
 public interface UserInfo {
     String getSubject();
     String getUsername();
+    String getName();
+    String getEmail();
+    boolean isEmailVerified();
 }
